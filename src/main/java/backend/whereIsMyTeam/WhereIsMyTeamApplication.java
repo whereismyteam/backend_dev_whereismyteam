b69dc1ea5df06b3a2c9339f2fa945ab1b0a29072
dabeen <ltea99@naver.com>
2022-01-29 07:55:30 +0900

    <:tada:>프로젝트 환경 세팅

--- src/main/java/backend/whereIsMyTeam/WhereIsMyTeamApplication.java
+++ src/main/java/backend/whereIsMyTeam/WhereIsMyTeamApplication.java
@@ -2,8 +2,10 @@ package backend.whereIsMyTeam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class WhereIsMyTeamApplication {
 
 	public static void main(String[] args) {
