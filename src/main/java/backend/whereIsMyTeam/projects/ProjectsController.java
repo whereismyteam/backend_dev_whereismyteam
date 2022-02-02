6f915cb304a97486328d3b7ad193911a424afa86
이채린 <cofls6581@naver.com>
2022-02-03 05:03:54 +0900

    🎉로깅 전략 추가

--- src/main/java/backend/whereIsMyTeam/projects/ProjectsController.java
+++ src/main/java/backend/whereIsMyTeam/projects/ProjectsController.java
@@ -1,9 +1,12 @@
 package backend.whereIsMyTeam.projects;
 
 import lombok.RequiredArgsConstructor;
+import org.mybatis.logging.Logger;
+import org.mybatis.logging.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class ProjectsController {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
 }
--- src/main/java/backend/whereIsMyTeam/user/UserController.java
+++ src/main/java/backend/whereIsMyTeam/user/UserController.java
@@ -1,9 +1,12 @@
 package backend.whereIsMyTeam.user;
 
 import lombok.RequiredArgsConstructor;
+import org.mybatis.logging.Logger;
+import org.mybatis.logging.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
 }
