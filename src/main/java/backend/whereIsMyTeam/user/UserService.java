1843348d3100e8472e55d384e3aae9fe2ddc08b5
dabeen <ltea99@naver.com>
2022-02-03 14:37:23 +0900

    :lipstick: Service 어노테이션 수정

--- src/main/java/backend/whereIsMyTeam/user/UserService.java
+++ src/main/java/backend/whereIsMyTeam/user/UserService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true) //읽기전용 클래스
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserService {
 }
