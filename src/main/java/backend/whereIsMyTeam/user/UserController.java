d696e5664fabd5841739a5e6f37c3b739daec83f
dabeen <ltea99@naver.com>
2022-02-05 07:55:51 +0900

    :rocket: login,Email 등 예외처리 클래스 추가

--- src/main/java/backend/whereIsMyTeam/user/UserController.java
+++ src/main/java/backend/whereIsMyTeam/user/UserController.java
@@ -1,12 +1,47 @@
 package backend.whereIsMyTeam.user;
 
+import backend.whereIsMyTeam.redis.dto.ReIssueRequestDto;
+import backend.whereIsMyTeam.result.SingleResult;
+import backend.whereIsMyTeam.security.dto.TokenResponseDto;
+import backend.whereIsMyTeam.user.dto.UserLoginRequestDto;
+import backend.whereIsMyTeam.user.dto.UserLoginResponseDto;
+import backend.whereIsMyTeam.user.service.ResponseService;
+import backend.whereIsMyTeam.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.logging.Logger;
 import org.mybatis.logging.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+    private final ResponseService responseService;
+
     final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /*
+    * 로컬 로그인 API
+    */
+    @PostMapping("/login")
+    public SingleResult<UserLoginResponseDto> login(@RequestBody UserLoginRequestDto requestDto) {
+        UserLoginResponseDto responseDto = userService.loginUser(requestDto);
+        return responseService.getSingleResult(responseDto);
+    }
+
+    /*
+    * 'Refresh 토큰' 이용해 토큰 재발급 API
+    */
+    @PostMapping("/newAccessToken")
+    public SingleResult<TokenResponseDto> reIssue(@RequestBody ReIssueRequestDto requestDto) {
+        TokenResponseDto responseDto = userService.reIssue(requestDto);
+        return responseService.getSingleResult(responseDto);
+    }
 }
